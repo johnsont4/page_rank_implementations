493578994b615339c0b91594d6c8c6de3ab4e33f
Teagan Johnson <johnsont4@carleton.edu>
2022-03-13 21:28:24 -0500

    new graphs

--- matrix_implementation.py
+++ matrix_implementation.py
@@ -23,22 +23,33 @@ def run_matrix_page_rank(graph):
     diff = math.inf
     iter = 0
     old_matrix = v
-    while diff > .01:
-        print(diff)
+    while diff > .001:
+        #print(diff)
         v = np.matmul(A, v)
-        print(v)
+        #print(v)
 
         diff_matrix = abs(np.subtract(old_matrix, v))
         diff = np.sum(diff_matrix)
         old_matrix = v
         iter += 1
 
-    print("Num iteration: ", iter)
+    return np.round(v, 3), iter
 
 
 def main():
-    graph = Graph()
-    initialize_graph(graph, 'graph.txt')
-    run_matrix_page_rank(graph)
+    small_graph = Graph()
+    med_graph = Graph()
+    big_graph = Graph()
+    initialize_graph(small_graph, 'small_graph.txt')
+    initialize_graph(med_graph, 'med_graph.txt')
+    initialize_graph(big_graph, 'big_graph.txt')
+
+    small_v, small_iter = run_matrix_page_rank(small_graph)
+    med_v, med_iter = run_matrix_page_rank(med_graph)
+    big_v, big_iter = run_matrix_page_rank(big_graph)
+
+    print("Matrix: ", small_v, "Iterations: ", small_iter)
+    print("Matrix: ", med_v, "Iterations: ", med_iter)
+    print("Matrix: ", big_v, "Iterations: ", big_iter)
 
 main()
